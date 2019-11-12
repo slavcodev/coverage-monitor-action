00df472933394c1815315162a5fde86bbf0e8cb9
Veaceslav Medvedev <slavcopost@gmail.com>
2019-11-12 16:30:58 +0100

    Add Jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// https://jestjs.io/docs/en/configuration.html
+module.exports = {
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'logs',
+  coverageReporters: ['clover', 'html'],
+};
