73cb279525cfb055e773b838946e706d64989739
Veaceslav Medvedev <slavcopost@gmail.com>
2022-02-26 22:18:04 +0100

    Install typescript and dev tools

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// https://prettier.io/docs/en/configuration.html
+// https://prettier.io/docs/en/options.html
+// eslint-disable-next-line import/no-commonjs
+// Keep same in .eslintrc.js
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  arrowParens: 'always',
+};
--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,23 @@
 // https://jestjs.io/docs/en/configuration.html
+// eslint-disable-next-line import/no-commonjs
 module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
   verbose: true,
   collectCoverage: true,
   coverageDirectory: '.coverage',
-  coveragePathIgnorePatterns: ['tests'],
+  collectCoverageFrom: [
+    '**/src/**/*.ts',
+    "!**/node_modules/**",
+    "!**/tests/**"
+  ],
   // https://istanbul.js.org/docs/advanced/alternative-reporters/
+  // https://github.com/istanbuljs/istanbuljs/tree/master/packages/istanbul-reports/lib
   // coverageReporters: ['clover', 'html', 'json-summary', 'json', 'cobertura', 'teamcity', 'text-summary', 'text'],
   coverageReporters: ['clover', 'json-summary', 'html'],
+  coveragePathIgnorePatterns: ['tests'],
 };
